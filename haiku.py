36a53bc6d8664e66d4947001ca5c4e3892cf72a6
Colin Knox <colinknox@66405s-iMac.rule.lan>
2024-04-26 10:44:39 -0400

    Adding starting project.

---
+++ haiku.py
@@ -0,0 +1,15 @@
+print('''o butterfly,
+what are you dreaming there,
+fanning your wings.
+      
+- Fukuda Chiyo-ni''')
+
+flying_animal = input("Let's change this haiku up a little to make it your own! What's your favorite flying animal? ")
+progressive_verb = input("Now we need an act that ends in -ing (known as a progressive verb). Have any in mind? ")
+appendage = input("Last, think of an appendage. ")
+
+print = print(f'''o {flying_animal},
+what are you {progressive_verb} there,
+fanning your {appendage}.
+      
+- Fukuda Chiyo-ni''')
