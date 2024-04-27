fe89e3ef02e0fa65f385116b30b3a8f42d22e767
Colin Knox <colinwknox@gmail.com>
2024-04-26 21:42:00 -0400

    Add input for user's name. Change poet's name with user's.

--- haiku.py
+++ haiku.py
@@ -7,9 +7,10 @@ fanning your wings.
 flying_animal = input("Let's change this haiku up a little to make it your own! What's your favorite flying animal? ")
 progressive_verb = input("Now we need an act that ends in -ing (known as a progressive verb). Have any in mind? ")
 appendage = input("Last, think of an appendage. ")
+name = input("Finally, what your name? ")
 
 print = print(f'''o {flying_animal},
 what are you {progressive_verb} there,
 fanning your {appendage}.
       
-- Fukuda Chiyo-ni''')
+- {name}''')
